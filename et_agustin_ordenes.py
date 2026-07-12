81f419a5b5e60ff954f6797917db1d9ce84ca8c9
Agustín Ordenes <ag.ordenes@duocuc.cl>
2026-07-12 16:17:20 -0400

    Creacion de funciones para el menu

--- et_agustin_ordenes.py
+++ et_agustin_ordenes.py
@@ -21,3 +21,75 @@ reservas = {
 'H005': [210, 5],
 'H006': [450, 4],
 }
+
+def leer_opcion():
+    print('''========= GRIMORIO ASTRALIS =========
+1. Pergaminos por escuela de magia
+2. Búsqueda de hechizos por rango de precio
+3. Actualizar precio de hechizo
+4. Agregar hechizo
+5. Eliminar hechizo
+6. Salir
+====================================''')
+    try:
+        validacion=int(input("Ingrese una opcion: "))
+        if validacion==0:
+            return validacion
+        elif validacion<1 or validacion>6:
+            print("Debe seleccionar una opcion valida")
+            return 0
+        return validacion
+    except ValueError:
+        print("Debe seleccionar una opcion valida")
+        return 0
+def pergaminos_escuela(escuela,hechizos,reservas):   
+    print(f"Pergaminos disponibles para la escuela de magia {escuela}: ")
+    cant_pregaminos_escuela=0
+    for codigo,datos in hechizos.items():
+        if datos[1].lower() == escuela.lower():
+            cant_pregaminos_escuela+=reservas[codigo][1]
+    print(f"Total de pergaminos de {escuela} son: {cant_pregaminos_escuela}")
+def busqueda_precio(p_min, p_max,hechizos,reservas):
+    contador=1
+    cantidad_de_hechizos_mostrados=0
+    for codigo,datos in reservas.items():
+        if p_min<datos[0]<p_max and datos[1]>0:
+            print(f"{contador}. {hechizos[codigo][0]} | Precio: {datos[0]} | Stock: {datos[1]}")
+            contador+=1
+            cantidad_de_hechizos_mostrados+=1
+    if cantidad_de_hechizos_mostrados==0:
+        print(f"No hay hechizos disponibles en el rango de precios de {p_min} - {p_max} cristales")
+def main():
+    while True:
+        op_menu=leer_opcion()
+        if op_menu==0:
+            print("Debe seleccionar una opcion valida")
+        elif op_menu==1:
+            escuela=input("Ingrese el nombre de la escuela de magia que desee revisar: ")
+            pergaminos_escuela(escuela,hechizos,reservas)
+        elif op_menu==2:
+            while True:
+                try:
+                    p_min=int(input("Ingrese su monto minimo a pagar: "))
+                    if p_min>0:
+                        p_max=int(input("Ingrese su monto maximo a pagar:"))
+                        if p_max>0 and p_max>p_min:
+                            busqueda_precio(p_min,p_max,hechizos,reservas)
+                            break
+                        print("Debe ingresar valores enteros y su monto maximo tiene que ser un valor ams grande que su monto minimo")
+                    print("Debe ingresar valores enteros")
+                except ValueError:
+                    print("Debe ingresar valores enteros")
+        elif op_menu==3:
+            print()
+        elif op_menu==4:
+            print()
+        elif op_menu==5:
+            print()
+        else:
+            print("Saliendo del sistema")
+            break
+main()
+
+
+
